3de7f62df4b65e3cb2bc9a7aaa232d039c6879f2
Mustapha Tijani <thenewxpat@gmail.com>
2026-07-08 22:40:24 +0100

    first commit

---
+++ src/main/java/com/tijanidevit/phylx/PhylxApplication.java
@@ -0,0 +1,13 @@
+package com.tijanidevit.phylx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhylxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhylxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tijanidevit/phylx/PhylxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tijanidevit.phylx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhylxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
